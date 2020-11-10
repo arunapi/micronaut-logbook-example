08732f866df5e1040d8074da288f80bef7feeda9
Arun Abraham <arunapi@gmail.com>
2020-11-10 12:54:34 -0500

    Created app using mn cli

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # micronaut-logbook-example
+
+Created project using mn cli
+```shell script
+mn create-app --jdk=14 --test=spock micronaut-logbook-example
+```
+
+
---
+++ src/test/groovy/micronaut/logbook/example/MicronautLogbookExampleSpec.groovy
@@ -0,0 +1,19 @@
+package micronaut.logbook.example
+
+import io.micronaut.runtime.EmbeddedApplication
+import io.micronaut.test.extensions.spock.annotation.MicronautTest
+import spock.lang.Specification
+import javax.inject.Inject
+
+@MicronautTest
+class MicronautLogbookExampleSpec extends Specification {
+
+    @Inject
+    EmbeddedApplication<?> application
+
+    void 'test it works'() {
+        expect:
+        application.running
+    }
+
+}
